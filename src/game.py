54c0234b8d5735cdce86df561e21614bd8ce4f56
Jacob Pieczynski <jpieczynski2021@gmail.com>
2024-01-24 00:06:10 -0700

    added temp 'game' stats to each player type

--- src/game.py
+++ src/game.py
@@ -4,7 +4,8 @@ class Game:
     def __init__(self, data):
         self.reset_stats()
         self.info_lines, self.start_lines, self.pbp_lines, self.data_lines = [], [], [], []
-        self.home_lineup, self.visitor_lineup = [], [] # Array linking to the player object
+        self.home_lineup, self.visitor_lineup = dict(), dict() # Dicts linking to the player object
+        self.home_fielders, self.visitor_fielders = [None] * 11, [None] * 11 # Ordered by position map e.g. home_fielders[2] will represent the catcher, [10] will rep DH, [1] pitcher
         self.sort_info(data)
         self.GameLog = GAMELOG[self.year][self.id] # Links to GameLog for this game
         self.GameLog.connect_game(self)
@@ -44,28 +45,82 @@ class Game:
             line = line.split(',')
             playerid = line[1]
             is_home = int(line[3]) == 0
-            pos = int(line[4])
+            bat_pos = int(line[4])
+            pos = int(line[5])
             # Pitchers
-            if pos == 0 and is_home:
+            if bat_pos == 0 and is_home:
                 self.home_starter = PITCHERS[playerid]
-            elif pos == 0:
+                self.home_starter.reset_temp_stats()
+                self.home_fielders[pos] = PITCHERS[playerid]
+            elif bat_pos == 0:
                 self.visitor_starter = PITCHERS[playerid]
+                self.visitor_starter.reset_temp_stats()
+                self.visitor_fielders[pos] = PITCHERS[playerid]
             # Batting starting lineup
             elif is_home:
-                self.home_lineup.append(PLAYERS[playerid])
+                self.home_lineup[playerid] = PLAYERS[playerid]
+                PLAYERS[playerid].reset_temp_stats()
+                self.home_fielders[pos] = PLAYERS[playerid]
             else:
-                self.visitor_lineup.append(PLAYERS[playerid])
+                self.visitor_lineup[playerid] = PLAYERS[playerid]
+                PLAYERS[playerid].reset_temp_stats()
+                self.visitor_fielders[pos] = PLAYERS[playerid]
 
     def simulate_game(self):
+        self.reset_sim_attributes()
         # Initial variables
-        pass
+        for line in self.pbp_lines:
+            if line.startswith('play,'):
+                info = line.split(',')
+                home_at_bat = bool(info[2] == '0')
+                #print(line)
+                #print(home_at_bat)
+                id = info[3]
+                pitch_count = info[4]
+                pitches = info[5]
+                play = info[6]
+                # Change to one "Batters" dict?
+                if home_at_bat:
+                    print(self.home_lineup)
+                    if id not in self.home_lineup:
+                        self.home_lineup[id] = PLAYERS[id]
+                    self.current_batter = self.home_lineup[id]
+                else:
+                    if id not in self.visitor_lineup:
+                        self.visitor_lineup[id] = PLAYERS[id]
+                    self.current_batter = self.visitor_lineup[id]
+                self.current_batter.add_temp_stat('PA', 1)
+                self.current_pitcher.add_temp_stat('BF', 1)
+                # MISC LOGIC
+                # ...
+                # ...
+                # Swaps pitchers at the end of the inning
+                if self.inn_outs == 3:
+                    if home_at_bat:
+                        self.current_pitcher = self.home_pitcher
+                    else:
+                        self.current_pitcher = self.visitor_pitcher
+                    self.inn_outs = 0
+            elif line.startswith('sub,'):
+                pass
 
-    def clear_bases(self):
-        self.bases = [0, 0, 0] # 1st 2nd 3rd
+    def reset_sim_attributes(self):
+        self.bases = [None, None, None]
+        self.inn_outs = 0
+        self.game_outs = 0
+        self.inning = 1
+        """
+        self.current_batter = self.home_lineup[0]
+        self.next_home_batter = self.home_lineup[1]
+        self.next_visitor_batter = self.visitor_lineup[0]
+        """
+        self.current_pitcher = self.visitor_starter
+        self.visitor_pitcher = self.current_pitcher
+        self.home_pitcher = self.home_starter
 
     def reset_stats(self):
         # Add all variables here to reset
         pass
 
     def __repr__(self):
-        return f"Game Object for {self.id} - {self.visitor} at {self.home} on {self.date}"
+        return f"Game Object for {self.id} - {self.visitor} at {self.home} on {self.date}"#\nHome Starting lineup: {self.home_lineup}, Visitor Starting lineup: {self.visitor_lineup}\n Home Fielding Order {self.home_fielders}, Visitor fielders {self.visitor_fielders}"
--- src/pitcher.py
+++ src/pitcher.py
@@ -10,9 +10,36 @@ class Pitcher():
         self.team = info[5]
         self.pos = info[6]
         self.reset_stats()
+        self.reset_temp_stats()
+
+    def add_temp_stat(self, stat, quantity):
+        if stat not in self.temp_stats:
+            print(f'{stat} not in {self.__repr__()}\'s stats.')
+            return False
+        self.temp_stats[stat] += quantity
+
+    def inc_total_stats(self):
+        for stat in self.temp_stats:
+            if stat == 'OP':
+                self.totals['IP'] += self.convert_op_ip(self.temp_stats['OP'])
+            self.totals[stat] += self.temp_stats[stat]
 
     def reset_stats(self):
-        self.appearances = 0
+        self.totals = {'Appearances': 1, 'Wins': 0, 'Losses': 0, 'Starts': 0, 'ER': 0, 'OP': 0, 'IP': 0, 'SOs': 0, 'Hits': 0, 'Walks': 0, 'HRs': 0, 'HBP': 0, 'BF': 0}
+
+    def reset_temp_stats(self):
+        # Add wild pitches, intentional walks, batters faced? More specific stats like # of popouts, strikes looking, etc.? Calc GameScore stat?
+        self.temp_stats = {'Appearances': 1, 'Wins': 0, 'Losses': 0, 'Starts': 0, 'ER': 0, 'OP': 0, 'SOs': 0, 'Hits': 0, 'Walks': 0, 'HRs': 0, 'HBP': 0, 'BF': 0}
+
+    # Convert outs pitched to the more commonly used "Innings Pitched"
+    def convert_op_ip(op):
+        # 7 = 2 1/3
+        partial = op % 3
+        return ((op - partial) / 3) + round(partial / 3, 2)
+    
+    def get_pitching_totals(self):
+        # Use totals and loop through games like we did before
+        pass
 
     def __repr__(self):
         return f'Pitcher object of {self.name} on {self.team} with id {self.id}'
