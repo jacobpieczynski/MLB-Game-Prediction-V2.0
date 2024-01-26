662ef80f20f98d238e6b70365583e99c4e1fed2d
Jacob Pieczynski <jpieczynski2021@gmail.com>
2024-01-26 14:56:43 -0700

    added sub function and start of play function

--- src/game.py
+++ src/game.py
@@ -1,11 +1,14 @@
 from const import *
+from pitcher import Pitcher
+from player import Player
 
 class Game:
     def __init__(self, data):
         self.sort_data(data)
 
     def __repr__(self):
-        pass
+        if self.id:
+            return f'Game object: {self.id} on {self.date} - {self.info["visteam"]} at {self.info["hometeam"]}'
 
     # 4 different categories of data that must be treated differently
     def sort_data(self, data):
@@ -69,10 +72,60 @@ class Game:
         for line in self.plays:
             line = line.split(',')
             # 3 types of play info
-            if line[0] == 'info':
-                pass
-            elif line[0] in ['sub', 'badj', 'radj']:
-                pass
+            if line[0] == 'play' and line[-1] != 'NP': #TODO: NP will always (?) preceed a sub/adj, can ignore those lines
+                #['play', '1', '0', 'sprig001', '32', 'SBFBBX', '7/F7LD']
+                #print(line)
+                #print(self.id)
+                is_home = int(line[2]) == 0
+                playerid = line[3]
+                pitch_count = line[4]
+                pitches_thrown = line[5]
+                play = line[6:]
+                batter = PLAYERS[playerid]
+                if is_home:
+                    pitcher = self.visitor_lineup[1]
+                else:
+                    pitcher = self.home_lineup[1]
+            elif line[0] == 'sub':
+                # TODO: The numbers are in the standard notation, with designated hitters being identified as position 10. On sub records 11 indicates a pinch hitter and 12 is used for a pinch runner. When a player pinch hits or pinch runs for the DH, that player automatically becomes the DH, so no 'sub' record is included to identify the new DH.
+                #['sub', 'gonzv001', '"Victor Gonzalez"', '1', '0', '1']
+                playerid = line[1]
+                playername = line[2]
+                is_home = int(line[3]) == 0
+                field_pos = int(line[5])
+                # TODO: TEMP - adjust to account for pinch runners and hitters
+                if field_pos >= 10:
+                    field_pos = 10
+                player = None
+                # The player is being subbed as a pitcher
+                if field_pos == 1:
+                    # TODO: End prior pitcher outing
+                    # Accounts for when a pitcher bats (unusual)
+                    if int(line[4]) != 0:
+                        if playerid in PLAYERS:
+                            player = PLAYERS[playerid]
+                        else:
+                            player = PLAYERS[playerid] = Player(f'{playerid},{playername.split(" ")[1]},{playername.split(" ")[0]},,,,{field_pos}')
+                    else:
+                        # Accounts for when a player pitches (unusual)
+                        if playerid in PITCHERS:
+                            player = PITCHERS[playerid]
+                        else:
+                            player = PITCHERS[playerid] = Pitcher(f'{playerid},{playername.split(" ")[1]},{playername.split(" ")[0]},,,,{field_pos}')
+                # Otherwise, the player is being subbed as a pitcher
+                else:
+                    # Accounts for when a pitcher is being subbed in as a batter
+                    if playerid not in PLAYERS:
+                        player = PLAYERS[playerid] = Player(f'{playerid},{playername.split(" ")[1]},{playername.split(" ")[0]},,,,{field_pos}')
+                    else:
+                        player = PLAYERS[playerid]
+                if is_home:
+                    self.home_lineup[field_pos] = player
+                else:
+                    self.visitor_lineup[field_pos] = player
             elif line[0] == 'data':
                 pass
-
+            else:
+                # TODO: Account for radj and badj
+                pass
+            
