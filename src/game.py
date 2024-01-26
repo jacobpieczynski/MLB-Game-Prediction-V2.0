6a73a9770eafda84e80b35892460c73e8cd04157
Jacob Pieczynski <jpieczynski2021@gmail.com>
2024-01-26 13:58:27 -0700

    to parse game info

--- src/game.py
+++ src/game.py
@@ -2,4 +2,38 @@ from const import *
 
 class Game:
     def __init__(self, data):
-        self.sort(data)
+        self.sort_data(data)
+
+    def __repr__(self):
+        pass
+
+    # 4 different categories of data that must be treated differently
+    def sort_data(self, data):
+        self.infos, self.start, self.plays, self.data = [], [], [], []
+        for line in data:
+            if line.startswith('info,') or line.startswith('id,') or line.startswith('version,'):
+                self.infos.append(line)
+            elif line.startswith('start,'):
+                self.start.append(line)
+            elif line.startswith('play') or line.startswith('sub') or line.startswith('com') or line.startswith('badj,') or line.startswith('radj,'):
+                self.plays.append(line)
+            elif line.startswith('data'):
+                self.data.append(line)
+            else:
+                print('Line type not found:')
+                print(line)
+        self.parse_info()
+
+    # Takes the info metadata and parses it. Creates a game id and metadata
+    def parse_info(self):
+        info = dict()
+        for line in self.infos:
+            line = line.split(',')
+            if line[0] == 'id':
+                self.date = line[1][3:-1]
+            elif line[0] != 'version':
+                info[line[1]] = line[2]
+        self.id = self.date + info['hometeam'] + info['visteam']
+        self.info = info
+        self.GameLog = GAMELOG[self.date[:4]][self.id]
+
