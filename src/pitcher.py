e49fe6df98a8849bd46cee0db3e55ed2d8954d9b
Jacob Pieczynski <jpieczynski2021@gmail.com>
2024-01-26 13:33:24 -0700

    pitchers init()

--- src/pitcher.py
+++ src/pitcher.py
@@ -2,4 +2,11 @@ from const import *
 
 class Pitcher:
     def __init__(self, info):
+        info = info.split(',')
+        self.id = info[0]
+        self.name = f'{info[2]} {info[1]}'
+        self.bats = info[3]
+        self.throws = info[4]
+        self.team = info[5]
+        self.pos = info[6]
         print(info)
