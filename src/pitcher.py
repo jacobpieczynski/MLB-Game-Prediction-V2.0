4e0da6f3296d84658e186c4f21342b9a549d0971
Jacob Pieczynski <jpieczynski2021@gmail.com>
2024-01-26 14:19:04 -0700

    basic pitcher stats

--- src/pitcher.py
+++ src/pitcher.py
@@ -10,5 +10,31 @@ class Pitcher:
         self.team = info[5]
         self.pos = info[6]
 
+    # Gets the player statistics between two dates
+    def get_totals(self, start_date, end_date):
+        pass
+
+    # Alters a given stat
+    def inc_game_stat(self, stats: list, quantities: list):
+        if len(stats) != len(quantities):
+            print('Invalid number of stats compared to quantities, player inc_game_stat')
+            return False
+        for stat, quantity in stats, quantities:
+            self.game_stats[stat] += quantities[quantity]
+
+    # Adds all game stats to 'perm' stats
+    def add_game_stats(self):
+        for stat in self.game_stats:
+            self.stats[stat] += self.game_stats[stat]
+
+    # Reset Functions:
+    def reset_stats(self):
+        for stat in self.stats:
+            self.stats[stat] = 0
+
+    def reset_game_stats(self):
+        for stat in self.game_stats:
+            self.game_stats[stat] = 0
+
     def __repr__(self):
         return f'Pitcher object {self.name}, a {self.pos} for {self.team} - ID: {self.id}'
