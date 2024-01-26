ed0c7628c74baecf2565eea2e6f470d1262a8c49
Jacob Pieczynski <jpieczynski2021@gmail.com>
2024-01-26 13:31:54 -0700

    player init()

--- src/player.py
+++ src/player.py
@@ -2,4 +2,11 @@ from const import *
 
 class Player:
     def __init__(self, info):
-        pass
+        info = info.split(',')
+        self.id = info[0]
+        self.name = f'{info[2]} {info[1]}'
+        self.bats = info[3]
+        self.throws = info[4]
+        self.team = info[5]
+        self.pos = info[6]
+        print(info)
