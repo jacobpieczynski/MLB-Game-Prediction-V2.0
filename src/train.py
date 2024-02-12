55ea6e64b5562ef7fcf409194ec6f0cef24907a0
Jacob Pieczynski <jpieczynski2021@gmail.com>
2024-02-12 13:36:22 -0700

    misc testing

--- src/train.py
+++ src/train.py
@@ -7,7 +7,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 
 # Load the CSV file into a DataFrame
-data = pd.read_csv('stats.csv').drop(columns=['Date', 'Home', 'Visitor', 'GameID'], axis=1)
+data = pd.read_csv('stats.csv').drop(columns=['Date', 'Home', 'Visitor', 'GameID', 'RPGDiff', 'WinDiff', 'RADiff'], axis=1)
 correlation_matrix = data.corr()
 target_correlation = correlation_matrix['HWin'].abs().sort_values(ascending=False)
 
@@ -16,10 +16,10 @@ print(target_correlation)
 X = data.drop('HWin', axis=1)
 y = data['HWin']
 
-model_average, model_best, rf_model_average = 0, 0, 0
+model_average, model_best, model_min, rf_model_average = 0, 0, 1, 0
 rng = 100
 for i in range(rng):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1) #random_state=705
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2) #random_state=705
 
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)
@@ -37,10 +37,14 @@ for i in range(rng):
     model_average += acc
     if acc >= model_best:
         model_best = acc
+    elif acc <= model_min:
+        model_min = acc
     print(f"TRIAL {i + 1} - LR Accuracy: {acc}")
 
 model_average /= rng
 rf_model_average /= rng
 
 print(f"Best accuracy is {model_best}")
+print(f'Min accuracy is {model_min}')
+print(f'Accuracy range is {round(model_best - model_min, 8)}')
 print(f"Overall accuracy Linear Regression is: {model_average}")
